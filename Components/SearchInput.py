c96845d5042f9f64d2704d89e0fafd5802753afc
emilycodesoft <emilycodesoft@gmail.com>
2023-06-10 17:58:39 -0500

    feat: searching a book implemented

--- Components/SearchInput.py
+++ Components/SearchInput.py
@@ -16,10 +16,19 @@ class SearchInput(UserControl):
     def search_input_instance(self):
         add_control_reference("SearchInput", self)
 
-    def textbox_changed(e):
-        pass
+    def filter_books_table(self, e):
+        for key, value in control_map.items():
+            if key == "BooksTable":
+                if (len(value.controls[0].controls[0].rows)) != 0:
+                    for data in value.controls[0].controls[0].rows[:]:
+                        if e.data.lower() in data.cells[1].content.value.lower():
+                            data.visible = True
+                            data.update()
+                        else:
+                            data.visible = False
+                            data.update()
 
     def build(self):
         self.search_input_instance()
 
-        return TextField(label="Buscar Libro", on_change=self.textbox_changed)
+        return TextField(label="Buscar Libro", on_change=self.filter_books_table)
